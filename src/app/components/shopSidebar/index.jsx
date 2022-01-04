86cf516bd927094184244497304cb72cf034969a
Alex Brown <alexdbrown2526@gmail.com>
2022-01-03 22:51:43 -0600

    fixed sidebar

--- src/app/components/shopSidebar/index.jsx
+++ src/app/components/shopSidebar/index.jsx
@@ -13,7 +13,6 @@ export function ShopSidebar() {
           <ul class="pt-2 pb-4 space-y-1 text-sm">
             <li class="rounded-sm">
               <FontAwesomeIcon icon="fa-regular fa-campground" />
-              <path d="M469.666,216.45,271.078,33.749a34,34,0,0,0-47.062.98L41.373,217.373,32,226.745V496H208V328h96V496H480V225.958ZM248.038,56.771c.282,0,.108.061-.013.18C247.9,56.832,247.756,56.771,248.038,56.771ZM448,464H336V328a32,32,0,0,0-32-32H208a32,32,0,0,0-32,32V464H64V240L248.038,57.356c.013-.012.014-.023.024-.035L448,240Z"></path>
 
               <span>Backpacks</span>
             </li>
